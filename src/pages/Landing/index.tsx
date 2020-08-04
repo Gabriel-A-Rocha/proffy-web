33be3ecce33e7057eea76caadbf6bb825a2ea358
Gabriel-A-Rocha <gabriel.altair.rocha@gmail.com>
2020-08-03 23:34:14 -0300

    Implement page navigation with react-router-dom

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import logoImg from "../../assets/logo.svg";
 import landingImg from "../../assets/landing.svg";
@@ -21,15 +22,15 @@ function Landing() {
         <img src={landingImg} alt="Learning platform" className="hero-image" />
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Study" />
             Study
-          </a>
+          </Link>
 
-          <a href="" className="give-classes">
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Give classes" />
             Teach
-          </a>
+          </Link>
         </div>
 
         <span className="total-connections">
